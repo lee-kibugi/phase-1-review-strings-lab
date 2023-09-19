0918bcf9acf7b8b0e84f9f625fa69df77f49b973
linda wanjiku <lindashykooh@gmail.com>
2023-09-19 10:48:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser ='Linda Kibugi';
+
+const welcomeMessage = 'Welcome to Flatbook, ${Linda Kibugi}!';
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
